6fcaf8e79b8f1241f2a7608237fd97aab25094b3
kasojuraju <kasoju.raj@gmail.com>
2018-01-24 08:54:37 +0530

    mavengithubjenkins commit

---
+++ Sample_Maven/AppTest.java
@@ -0,0 +1,24 @@
+package com.test.Sample_Maven;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class AppTest{
+	
+	WebDriver d;
+	@BeforeClass
+	public void browserinit(){
+		System.setProperty("webdriver.chrome.driver", "D:\\Nag\\chromedriver.exe");
+		d=new ChromeDriver();
+	}
+	
+	@Test
+	public void sample_Test(){
+		d.get("https://www.javatpoint.com/java-arraylist");
+		System.out.println("Sample Test annotation for Maven, Github and Jenkins configuration");
+		d.findElement(By.xpath("//*[@id='smoothmenu1']/ul/li[6]/a")).click();
+	}
+}
